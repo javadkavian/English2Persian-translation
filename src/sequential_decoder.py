deccc3b068060f2aeafaaad83a073df667fb7247
javad <javadkavian8@gmail.com>
2024-09-18 13:41:58 +0330

    add:sequential decoder class

---
+++ src/sequential_decoder.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+
+
+
+class SequentialDecoder(nn.Sequential):
+    def forward(self, *inputs):
+        x, y, mask = inputs
+        for module in self._modules.values():
+            y = module(x, y, mask)
+        return y    
