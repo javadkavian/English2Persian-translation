05cfa2f244806c656e673c07f30f731d7d320976
javad <javadkavian8@gmail.com>
2024-09-23 23:15:57 +0330

    bug:there is a bug in sentence embedding module

---
+++ src/embedding/sentence_embedding.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+# from ..positional_encoding import PositionalEncoding
+
+import pandas as pd
+
+
+def tokenize(sentence,
+              start_token,
+                end_token,
+                  language2idx,
+                    max_sequence_len,
+                      START_TOKEN,
+                        END_TOKEN,
+                          PADDING_TOKEN):
+    word_indices = [language2idx[token] for token in list(sentence)]
+    if start_token:
+        word_indices.insert(0, language2idx[START_TOKEN])
+    if end_token:
+        word_indices.append(language2idx[END_TOKEN])
+    for _ in range(len(word_indices), max_sequence_len):
+        word_indices.append(language2idx[PADDING_TOKEN])
+
+    return torch.tensor(word_indices)            
+
+
+
+
+class SentenceEmbedding(nn.Module):
+
+    def __init__(self, max_sequence_len, model_dim, language2idx, START_TOKEN, END_TOKEN, PADDING_TOKEN):
+        super().__init__()
+        self.vocab_size = len(language2idx)
+        self.max_sequence_len = max_sequence_len
+        self.embedding = nn.Embedding(self.vocab_size, model_dim)
+        self.language2idx = language2idx
+        # self.positional_encoding = PositionalEncoding(model_dim, max_sequence_len)
+        self.dropout = nn.Dropout(p=.1)
+        self.START_TOKEN = START_TOKEN
+        self.END_TOKEN = END_TOKEN
+        self.PADDING_TOKEN = PADDING_TOKEN
+        self.device = torch.device('cuda')
+
+    def batch_tokenize(self, batch, start_token, end_token):
+        tokenized = []
+        for idx in range(len(batch)):
+            tokenized.append(tokenize(batch[idx], 
+                                      start_token, 
+                                      end_token, 
+                                      self.language2idx, 
+                                      self.max_sequence_len, 
+                                      self.START_TOKEN, 
+                                      self.END_TOKEN, 
+                                      self.PADDING_TOKEN)) 
+
+        tokenized = torch.stack(tokenized) 
+        return tokenized.to(self.device)
+
+
+    def forward(self, x, start_token, end_token):
+        x = self.batch_tokenize(x, start_token, end_token) 
+        x = self.embedding(x)
+        # pos = self.positional_encoding().to(self.device)
+        x = self.dropout(x)
+        return x
+
+
+
+
+if __name__ == "__main__":
+    START_TOKEN = ''
+    END_TOKEN = ''
+    PADDING_TOKEN = ''
+    english_vocabulary = [START_TOKEN, ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', 
+                        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                        ':', '<', '=', '>', '?', '@',
+                        '[', '\\', ']',
+                        '^', '_', '`', 
+                        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+                        'y', 'z', 
+                        '{', '|', '}', '~', PADDING_TOKEN, END_TOKEN
+                        ]
+    
+    persian_vocabulary = [
+    START_TOKEN, ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', 
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    ':', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', 
+    'آ', 'ا', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'ژ', 'س', 'ش', 
+    'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'و', 'ه', 'ی',
+    'ء', 'ۀ', 'ؤ', 'ي', 'ك', 'ة', '‌', 'ٔ', 'ى', PADDING_TOKEN, END_TOKEN
+    ]
+    index_to_persian = {k:v for k,v in enumerate(persian_vocabulary)}
+    persian_to_index = {v:k for k,v in enumerate(persian_vocabulary)}
+    index_to_english = {k:v for k,v in enumerate(english_vocabulary)}
+    english_to_index = {v:k for k,v in enumerate(english_vocabulary)}
+    df = pd.read_csv('././dataset/shortened_dataset.csv')
+    persian_sentences = df['persian'].to_list()
+    english_sentences = df['english'].to_list()
+    model = SentenceEmbedding(100, 256, english_vocabulary, START_TOKEN, END_TOKEN, PADDING_TOKEN)
+    out = model(english_sentences, True, True)
+    print(out.shape)
+
+
